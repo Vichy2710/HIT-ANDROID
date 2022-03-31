5c4efabb266f1196ac18d668f5b461fadecb91a6
unknown <trieuvino.1@gmail.com>
2022-03-31 16:29:07 +0700

    31-03-2022

---
+++ Buoi7/app/src/main/java/com/example/myapplication/CongViecAdapter.java
@@ -0,0 +1,79 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CongViecAdapter extends BaseAdapter {
+    private MainActivity context;
+    private int layout;
+    private List<CongViec> congViecList;
+
+    public CongViecAdapter(MainActivity context, int layout, List<CongViec> congViecList) {
+        this.context = context;
+        this.layout = layout;
+        this.congViecList = congViecList;
+    }
+
+    @Override
+    public int getCount() {
+        return congViecList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    private class ViewHolder{
+        TextView text;
+        ImageView edit, erase;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if(view==null){
+            holder = new ViewHolder();
+            LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(layout, null);
+            holder.text = view.findViewById(R.id.text);
+            holder.edit = view.findViewById(R.id.edit);
+            holder.erase = view.findViewById(R.id.erase);
+            view.setTag(holder);
+        }else{
+            holder=(ViewHolder) view.getTag();
+        }
+
+        CongViec congViec = congViecList.get(i);
+        holder.text.setText(congViec.getTen());
+
+        // bat su kien edit and erase
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.dialog_edit_note(congViec.getTen(), congViec.getId());
+            }
+        });
+
+        holder.erase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.dialog_erase_note(congViec.getId());
+            }
+        });
+        return view;
+    }
+}
---
+++ Buoi7/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Database database;
+
+    GridView gridViewCV;
+    ArrayList<CongViec> arrayListCV;
+    CongViecAdapter congViecAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridViewCV = findViewById(R.id.list);
+        arrayListCV = new ArrayList<>();
+
+        congViecAdapter = new CongViecAdapter(this, R.layout.one_note, arrayListCV);
+        gridViewCV.setAdapter(congViecAdapter);
+
+        // tao database
+        database = new Database(this, "GhiChu.sqlite", null, 1);
+
+        // tao bang
+        database.QueryData("CREATE TABLE IF NOT EXISTS CongViec(id INTEGER PRIMARY KEY AUTOINCREMENT, ten VARCHAR)");
+
+        // them du lieu
+        // database.QueryData("INSERT INTO CongViec VALUES(null, 'Lam bai android')");
+
+        getData();
+
+        RelativeLayout add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog_add_note();
+            }
+        });
+    }
+
+
+    private void dialog_add_note(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.add_note);
+
+        EditText editText = dialog.findViewById(R.id.add_note);
+        TextView add_btn = dialog.findViewById(R.id.btn_add);
+        TextView cancel_btn = dialog.findViewById(R.id.btn_cancel_add);
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String note = editText.getText().toString();
+                if(note.equals("")){
+                    dialog.dismiss();
+                }else{
+                    database.QueryData("INSERT INTO CongViec VALUES(null, '"+note+"')");
+                    dialog.dismiss();
+                    getData();
+                }
+            }
+        });
+
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    public void dialog_edit_note(String ten, int id){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.edit_note);
+
+        EditText editText = dialog.findViewById(R.id.edit_note);
+        TextView edit_btn = dialog.findViewById(R.id.btn_edit);
+        TextView cancel_btn = dialog.findViewById(R.id.btn_cancel_edit);
+
+        editText.setText(ten);
+        edit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newNote = editText.getText().toString().trim();
+                database.QueryData("UPDATE CongViec SET ten = '"+newNote+"' WHERE id='"+id+"'");
+                dialog.dismiss();
+                getData();
+            }
+        });
+
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void dialog_erase_note(int id){
+        AlertDialog.Builder eraseDialog = new AlertDialog.Builder(this);
+        eraseDialog.setMessage("Are you sure you want to delete?");
+        eraseDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                database.QueryData("DELETE FROM CongViec WHERE id='"+id+"'");
+                getData();
+            }
+        });
+        eraseDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        eraseDialog.show();
+    }
+
+    private void getData(){
+        // select data
+        Cursor dataCongViec = database.GetData("SELECT * FROM CongViec");
+        arrayListCV.clear();
+        while (dataCongViec.moveToNext()){
+            String ten = dataCongViec.getString(1);
+            int id= dataCongViec.getInt(0);
+            arrayListCV.add(new CongViec(id, ten));
+        }
+
+        congViecAdapter.notifyDataSetChanged();
+    }
+}
